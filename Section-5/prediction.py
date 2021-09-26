e381972aefad57719c5b6d33e30e4f45eddb7c5d
LoginDenied <adjovi1996@hotmail.com>
2021-09-26 18:49:31 +0800

    Add breakdown and multiclass testing

--- Section-5/prediction.py
+++ Section-5/prediction.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, f1_score
 
 cwd = os.getcwd()
 data = []
@@ -25,6 +25,8 @@ features_enc = OrdinalEncoder(
 features_enc.fit(features)
 encoded_features = features_enc.transform(features)
 
+print("Buying class breakdown")
+print(df["buying"].value_counts(), "\n")
 score = df["buying"].values.reshape(-1, 1)
 score_enc = OrdinalEncoder(
     categories = [
@@ -37,12 +39,22 @@ encoded_scores = [x[0] for x in encoded_scores]
 
 x_train, x_test, y_train, y_test = train_test_split(encoded_features, encoded_scores, test_size=0.8, random_state=0)
 
-clf = LogisticRegression(random_state=0).fit(x_train, y_train)
-y_pred = clf.predict(x_test)
-cnf_matrix = confusion_matrix(y_test, y_pred)
-print(cnf_matrix)
+best_clf = None
+best_f1 = None
+multi_classes = ["ovr", "multinomial"]
+for multi_class in multi_classes:
+    clf = LogisticRegression(multi_class=multi_class, random_state=0).fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
+    cnf_matrix = confusion_matrix(y_test, y_pred)
+    print("Confusion matrix when using: %s" %(multi_class))
+    print(cnf_matrix)
+    f1 = f1_score(y_test, y_pred, average="micro")
+    print("F1 score is: %f\n" %(f1))
+    if best_f1 == None or f1 > best_f1:
+        print("Using %s" %(multi_class))
+        best_clf = clf
 
 x_pred = features_enc.transform([["high", "4", "big", "high", "good"]])
-y_pred = clf.predict(x_pred)
+y_pred = best_clf.predict(x_pred)
 prediction = score_enc.inverse_transform([y_pred])
 print("Predicted buying price is: %s" %(prediction[0][0]))
