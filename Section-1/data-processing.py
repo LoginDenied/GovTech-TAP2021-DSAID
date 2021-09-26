7932c439d0c8f485f8131f6c55b837ea7c0bf618
LoginDenied <adjovi1996@hotmail.com>
2021-09-26 08:37:05 +0800

    Add processing script and processed datasets

---
+++ Section-1/data-processing.py
@@ -0,0 +1,25 @@
+import os
+import os.path
+import glob
+import pandas as pd
+import numpy as np
+
+cwd = os.getcwd()
+file_paths = glob.glob(os.path.join(cwd, "datasets", "raw", "*.csv"))
+for file_path in file_paths:
+    # Read file into dataframe
+    df = pd.read_csv(file_path)
+    # Remove rows that do not have a name
+    df["name"].replace("", np.nan, inplace=True)
+    df.dropna(subset=["name"], inplace=True)
+    # Split name into first name and last name
+    df[["first_name", "last_name"]] = df.name.str.split(" ", n=1, expand=True)
+    df = df.drop("name", axis=1)
+    # Cast price field to float for comparision and removing of pre-pended 0s
+    # This might need tweaking based on the possible error ranges for price
+    # and only currently works as removing empty names settle the erronous entries
+    df = df.astype({"price": "float"})
+    # Add above_100 field
+    df["above_100"] = np.where(df["price"] > 100, True, False)
+    # # Save output file
+    df.to_csv(os.path.join(cwd, "datasets", "processed", os.path.basename(file_path)))
