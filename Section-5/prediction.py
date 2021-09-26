89dc2733a8621b550371c0c70840a222abefbe25
LoginDenied <adjovi1996@hotmail.com>
2021-09-26 20:10:01 +0800

    Modified automates parameter testing and fixed f1 score bug

--- Section-5/prediction.py
+++ Section-5/prediction.py
@@ -41,18 +41,28 @@ x_train, x_test, y_train, y_test = train_test_split(encoded_features, encoded_sc
 
 best_clf = None
 best_f1 = None
+solvers = ["newton-cg", "sag", "saga", "lbfgs"]
 multi_classes = ["ovr", "multinomial"]
+penalties = {
+    "newton-cg": ["l2", "none"],
+    "sag": ["l2", "none"],
+    "saga": ["l2", "none"],
+    "lbfgs": ["l2", "none"]
+}
 for multi_class in multi_classes:
-    clf = LogisticRegression(multi_class=multi_class, random_state=0).fit(x_train, y_train)
-    y_pred = clf.predict(x_test)
-    cnf_matrix = confusion_matrix(y_test, y_pred)
-    print("Confusion matrix when using: %s" %(multi_class))
-    print(cnf_matrix)
-    f1 = f1_score(y_test, y_pred, average="micro")
-    print("F1 score is: %f\n" %(f1))
-    if best_f1 == None or f1 > best_f1:
-        print("Using %s" %(multi_class))
-        best_clf = clf
+    for solver in solvers:
+        for penalty in penalties[solver]:
+            clf = LogisticRegression(penalty=penalty, multi_class=multi_class, solver=solver, random_state=0).fit(x_train, y_train)
+            y_pred = clf.predict(x_test)
+            cnf_matrix = confusion_matrix(y_test, y_pred)
+            print("Confusion matrix when using %s, %s and %s" %(multi_class, solver, penalty))
+            print(cnf_matrix)
+            f1 = f1_score(y_test, y_pred, average="micro")
+            print("F1 score is: %f\n" %(f1))
+            if best_f1 == None or f1 > best_f1:
+                print("Using %s, %s and %s\n" %(multi_class, solver, penalty))
+                best_clf = clf
+                best_f1 = f1
 
 x_pred = features_enc.transform([["high", "4", "big", "high", "good"]])
 y_pred = best_clf.predict(x_pred)
